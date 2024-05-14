a7874c46680a35cfd676f1e20106fe1c45c4f465
Saif <saifakhtar023@gmail.com>
2024-05-15 00:27:17 +0530

    Update TaskList.js

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,30 +1,4 @@
-// import React from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { deleteTask } from "../redux/actions";
 
-// const TaskList = () => {
-//   const tasks = useSelector((state) => state.tasks);
-//   const dispatch = useDispatch();
-
-//   const handleDelete = (taskId) => {
-//     dispatch(deleteTask(taskId));
-//   };
-
-//   return (
-//     <div>
-//       <ul>
-//         {tasks.map((task) => (
-//           <li key={task.id}>
-//             {task.text}
-//             <button onClick={() => handleDelete(task.id)}>Delete</button>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default TaskList;
 
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
