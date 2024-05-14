680c6c57b8ea85e648a17bc84072d2ee2b8c2b5f
Saif <saifakhtar023@gmail.com>
2024-05-15 00:28:01 +0530

    Update reducers.js

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,48 +1,4 @@
-// const initialState = {
-//   tasks: [],
-// };
 
-// const rootReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case "ADD_TASK":
-//       return {
-//         ...state,
-//         tasks: [...state.tasks, action.payload],
-//       };
-//     case "DELETE_TASK":
-//       return {
-//         ...state,
-//         tasks: state.tasks.filter((task) => task.id !== action.payload),
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
-// export default rootReducer;
-
-// const initialState = {
-//   tasks: [],
-// };
-
-// const rootReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case "ADD_TASK":
-//       return {
-//         ...state,
-//         tasks: [...state.tasks, action.payload],
-//       };
-//     case "DELETE_TASK":
-//       return {
-//         ...state,
-//         tasks: state.tasks.filter((task) => task.id !== action.payload),
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
-// export default rootReducer;
 
 // reducers.js;
 const initialState = {
